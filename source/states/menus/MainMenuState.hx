ec7f688919f989c1b0d7576f468bba1631fda230
Erickoxd <93102304+Unwiel@users.noreply.github.com>
2022-09-14 18:26:09 -0500

    Update MainMenuState.hx

--- source/states/menus/MainMenuState.hx
+++ source/states/menus/MainMenuState.hx
@@ -270,14 +270,16 @@ class MainMenuState extends MusicBeatState
 		if (Progression.beatMainWeek)
 			FlxTween.color(resetText, 1, FlxColor.WHITE, FlxColor.YELLOW, {type: PINGPONG});
 			
-		#if android
-        addVirtualPad(UP_DOWN, A_B);
-        addPadCamera();
-        #end
+		
 
 		changeItem();
 
 		super.create();
+
+                #if android
+        addVirtualPad(UP_DOWN, A_B);
+        addPadCamera();
+        #end#
 	}
 
 	var selectedSomethin:Bool = false;
