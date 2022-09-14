045d06500f604286ae64182a4ccf511f3049090e
Erickoxd <93102304+Unwiel@users.noreply.github.com>
2022-09-14 18:40:16 -0500

    Update MainMenuState.hx

--- source/states/menus/MainMenuState.hx
+++ source/states/menus/MainMenuState.hx
@@ -279,7 +279,7 @@ class MainMenuState extends MusicBeatState
                 #if android
         addVirtualPad(UP_DOWN, A_B);
         addPadCamera();
-        #end#
+        #end
 	}
 
 	var selectedSomethin:Bool = false;
